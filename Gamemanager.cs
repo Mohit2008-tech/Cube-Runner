7ff2a28c731c1ed0d86fc64ebab4ea252c0b83a5
Mohit Aggarwal <83158393+Mohit2008-tech@users.noreply.github.com>
2022-05-25 09:01:34 +0530

    Add files via upload

---
+++ Gamemanager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+  void EndGame()
+  {
+      Debug.Log("Game Over");
+  }
+}
